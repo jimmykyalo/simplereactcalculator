597f93fa58ebec0e8e2a0ce2408cc7dbcbc18ea2
Jimmy Kyalo <jimm.kyalo@gmail.com>
2022-06-24 12:03:07 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+
 
 function App() {
+  const [previousValue, setPreviousValue] = useState('')
+  const [nextValue, setNextValue] = useState('')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="row">
+          <div className="col"><button className="digit">9</button></div>
+          <div className="col"><button className="digit">8</button></div>
+          <div className="col"><button className="digit">7</button></div>
+        </div>
     </div>
   );
 }
